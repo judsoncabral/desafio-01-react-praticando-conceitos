9498a89c2b1082aa6ab7a952c4604945aa8759be
Judson Cabral de Melo <judsoncabral1@hotmail.com>
2023-03-13 00:19:03 -0300

    feature: Inclusão da função de deletar e adicionar; e refatoração do header com numero da pagina para ter um tamanho definido;

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,5 +1,6 @@
 import styles from "./Task.module.css";
 import { Trash } from "phosphor-react";
+import { FormEvent } from "react";
 
 export interface TaskType {
   id: string;
@@ -9,17 +10,35 @@ export interface TaskType {
 
 interface TaskProps {
   task: TaskType;
+  onDeleteTask: (task: TaskType) => void;
+  onChangeIsCompletedTask: (task: TaskType) => void;
 }
-export function Task({ task }: TaskProps) {
+
+export function Task({
+  task,
+  onDeleteTask,
+  onChangeIsCompletedTask,
+}: TaskProps) {
+  function handleDeleteTask(event: FormEvent) {
+    event.preventDefault();
+    onDeleteTask(task);
+  }
+
+  function handleChangeIsCompletedTask(event: FormEvent) {
+    event.preventDefault();
+    onChangeIsCompletedTask(task);
+  }
+
   return (
     <div className={styles.task}>
       <input
         className={styles.taskSelect}
         type="radio"
         checked={task.isCompleted}
+        onClick={handleChangeIsCompletedTask}
       ></input>
       <div className={styles.taskContent}>{task.content}</div>
-      <button>
+      <button onClick={handleDeleteTask}>
         <Trash size={14} />
       </button>
     </div>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -18,12 +18,38 @@ export function TodoList() {
         "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
       isCompleted: true,
     },
+    {
+      id: uuidv4(),
+      content:
+        "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+      isCompleted: true,
+    },
   ]);
 
   function onAddTask(task: TaskType) {
     setTasks(tasks.concat(task));
   }
 
+  function onChangeIsCompletedTask(taskSelected: TaskType) {
+    tasks.map((task) => {
+      if (task.id == taskSelected.id) {
+        task.isCompleted = !task.isCompleted;
+        console.log(task);
+      }
+    });
+    setTasks(tasks);
+  }
+
+  function getCompletedTasks() {
+    const result = tasks.reduce(function (acumulador, taskAtual) {
+      if (taskAtual.isCompleted) {
+        return acumulador + 1;
+      } else {
+        return acumulador;
+      }
+    }, 0);
+    return result;
+  }
   function onDeleteTask(taskToDelete: TaskType) {
     const taskWithouDeleteOne = tasks.filter((task) => {
       return task.id != taskToDelete.id;
@@ -35,18 +61,36 @@ export function TodoList() {
     <div>
       <CreateTask onAddTask={onAddTask} />
       <div className={styles.todoList}>
-        <header className={styles.header}>
+        <header>
           <div className={styles.taskCreate}>
             <strong>Tarefas Criadas</strong>{" "}
             <div className={styles.result}> {tasks.length}</div>
           </div>
           <div className={styles.taskComplete}>
             <strong>Concluídas</strong>
-            <div className={styles.result}>2 de {tasks.length}</div>
+            <div className={styles.result}>
+              {getCompletedTasks()} de {tasks.length}
+            </div>
           </div>
         </header>
+        {tasks.length == 0 && (
+          <div className={styles.contentEmpty}>
+            <p>
+              <span>Você ainda não tem tarefas cadastradas</span>
+              Crie tarefas e organize seus itens a fazer
+            </p>
+          </div>
+        )}
+
         {tasks.map((task: TaskType) => {
-          return <Task key={task.id} task={task} />;
+          return (
+            <Task
+              key={task.id}
+              task={task}
+              onDeleteTask={onDeleteTask}
+              onChangeIsCompletedTask={onChangeIsCompletedTask}
+            />
+          );
         })}
       </div>
     </div>
